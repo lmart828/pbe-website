18495a82bc1d8b716ddf85b387164849c7b81803
Luis Martinez <hdcpro28@gmail.com>
2026-04-08 21:54:45 -0400

    switch navbar to sticky and fix transparency

--- app/layout.js
+++ app/layout.js
@@ -34,7 +34,7 @@ export default function RootLayout({ children }) {
           </div>
         </header>
 
-        <main className="page-shell">{children}</main>
+        <main>{children}</main>
       </body>
     </html>
   );
